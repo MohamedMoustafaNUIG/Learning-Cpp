6c3fef11944365ddb0eeed2ae45e184be78ad3c8
Mohamed Moustafa <mohamed.moustafa@xperi.com>
2020-01-17 17:02:25 +0000

    first commit

---
+++ Hello World/Hello World.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	//std::cout << "Hello World!\n";
+	int width(5);
+	int height{ 10 };
+	string name{"Hello World"};
+	int age{};
+	cout << name<< " " << age << endl;
+}
